e53bbe5e6fce779f9f2829ad26f7cf2a33020f4a
Chienyi Tsai <chienyi.tsai@llamasoft.com>
2019-04-14 16:40:48 +0100

    initial project version

--- ScrapeLucyData/Program.cs
+++ ScrapeLucyData/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace ScrapeLucyData
 {
@@ -10,6 +11,28 @@ namespace ScrapeLucyData
     {
         static void Main(string[] args)
         {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = 
+                web.Load("https://www.bol.com/nl/t/top-10-windows-laptops/N/42976/?page=1&sort=rank");
+
+            HtmlNode [] allproducts =
+                document.DocumentNode.SelectNodes("//div[@class='product-item__content']").ToArray();
+
+            HtmlNode[] titles =
+                document.DocumentNode.SelectNodes("//a[@data-test='product-title']").ToArray();
+
+            string title; 
+
+            foreach (HtmlNode item in titles)
+            {
+                //get product title
+                title = item.InnerText;
+                Console.WriteLine("product name: " + title);
+                //get product specs
+                
+                //options
+                //get product price
+            }
         }
     }
 }
